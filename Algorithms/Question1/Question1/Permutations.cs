1bc6f00ce4ab63ff9606e48f27c9862fc50e2753
vk0808 <vk4765712@gmail.com>
2021-08-18 13:44:53 +0530

    add: recursive method

---
+++ Algorithms/Question1/Question1/Permutations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question1
+{
+    public class Permutations
+    {
+        public class Recursive
+        {
+            public static List<string> listOfStrings = new List<string>();
+           
+            // Utility function to swap two characters in a character array
+            private static void swap(char[] ch, int i, int j)
+            {
+                char temp = ch[i];
+                ch[i] = ch[j];
+                ch[j] = temp;
+            }
+
+            // Recursive function to generate all permutations of a string
+            private static void permutations(char[] ch, int currentIndex)
+            {
+                if (currentIndex == ch.Length - 1)
+                {
+                    // convert char to string and store in list
+                    string str = ch[0].ToString();
+                    for (var i=1; i < ch.Length; i++)
+                    {
+                        str += ch[i].ToString();
+                    }
+                    listOfStrings.Add(str);
+                }
+
+                for (int i = currentIndex; i < ch.Length; i++)
+                {
+                    swap(ch, currentIndex, i); 
+                    permutations(ch, currentIndex + 1);
+                    swap(ch, currentIndex, i); // to get back the original combination of string as before
+                }
+            }
+
+            // generate all permutations of a string
+            public static string[] perform(string s )
+            {
+                permutations(s.ToCharArray(), 0);
+                return listOfStrings.ToArray();
+            }
+        }
+    }
+}
--- Algorithms/Question1/Question1/Program.cs
+++ Algorithms/Question1/Question1/Program.cs
@@ -6,7 +6,17 @@ namespace Question1
     {
         static void Main(string[] args)
         {
+            // welcome message
             Console.WriteLine("Permutations of a String using Iterative method and Recursion method\n");
+
+            // object instansiation
+            string[] recursive = Permutations.Recursive.perform("ABC");
+            
+            // display result
+            foreach (var item in recursive)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
