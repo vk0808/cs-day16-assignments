eea0a23f6268c65bc16d6170f9f117873711ebc1
vk0808 <vk4765712@gmail.com>
2021-08-19 21:34:03 +0530

    add: welcome message

---
+++ Algorithms/Question5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merge Sort\n");
+        }
+    }
+}
