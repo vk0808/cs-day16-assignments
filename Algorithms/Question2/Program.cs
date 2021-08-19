300dbfe2df7dff0909ac80f7dfaaf4c23ca5a8ed
vk0808 <vk4765712@gmail.com>
2021-08-19 15:21:41 +0530

    fix: display

--- Algorithms/Question2/Program.cs
+++ Algorithms/Question2/Program.cs
@@ -8,6 +8,29 @@ namespace Question2
         static void Main(string[] args)
         {
             Console.WriteLine("Binary Search the Word from Word List\n");
+
+            // Path to txt file
+            const string PATH = "../../../WordList.txt";
+
+            // get array of words separated by ',' from the file
+            string[] words = File.ReadAllText(PATH).Split(",");
+
+            // sort the array
+            Array.Sort(words, StringComparer.InvariantCulture);
+            
+            // display the array
+            Array.ForEach(words, item => Console.Write(item + " "));
+
+            Console.WriteLine("\n");
+            // get the word to search from the user
+            Console.Write("Enter the word you want to search: ");
+            string searchKey = Console.ReadLine();
+
+            // object instantisation
+            BinarySearch bs = new BinarySearch(searchKey, words);
+
+            // call search method
+            bs.performSearch();
         }
     }
 }
