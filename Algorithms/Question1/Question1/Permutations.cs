da5e0e06a659ba3edee6296591bc8400513d1ea8
vk0808 <vk4765712@gmail.com>
2021-08-19 11:49:09 +0530

    add: iterative method

--- Algorithms/Question1/Question1/Permutations.cs
+++ Algorithms/Question1/Question1/Permutations.cs
@@ -9,7 +9,7 @@ namespace Question1
         public class Recursive
         {
             public static List<string> listOfStrings = new List<string>();
-           
+
             // Utility function to swap two characters in a character array
             private static void swap(char[] ch, int i, int j)
             {
@@ -19,31 +19,77 @@ namespace Question1
             }
 
             // Recursive function to generate all permutations of a string
-            private static void permutations(char[] ch, int currentIndex)
+            private static void permutationsRec(char[] ch, int currentIndex)
             {
                 if (currentIndex == ch.Length - 1)
                 {
                     // convert char to string and store in list
-                    string str = ch[0].ToString();
-                    for (var i=1; i < ch.Length; i++)
-                    {
-                        str += ch[i].ToString();
-                    }
+                    string str = new string(ch);
                     listOfStrings.Add(str);
                 }
 
                 for (int i = currentIndex; i < ch.Length; i++)
                 {
-                    swap(ch, currentIndex, i); 
-                    permutations(ch, currentIndex + 1);
+                    swap(ch, currentIndex, i);
+                    permutationsRec(ch, currentIndex + 1);
                     swap(ch, currentIndex, i); // to get back the original combination of string as before
                 }
             }
 
             // generate all permutations of a string
-            public static string[] perform(string s )
+            public static string[] performRec(string s)
             {
-                permutations(s.ToCharArray(), 0);
+                permutationsRec(s.ToCharArray(), 0);
+                return listOfStrings.ToArray();
+            }
+        }
+
+
+        public class Iterative
+        {
+            public static List<string> listOfStrings = new List<string>();
+
+            // function to return factorial of a number
+            private static int factorial(int num)
+            {
+                int val = 1;
+                for (int i = 2; i <= num; i++) {val *= i;}
+                return val;
+            }
+
+
+            // iterative function to generate all permutations of a string
+            private static void permutationsIter(string s)
+            {
+                int n = s.Length;
+                int fact = factorial(n);
+
+                for (int i = 0; i < fact; i++)
+                {
+                    StringBuilder sb = new StringBuilder(s);
+                    int temp = i;
+                    char[] ch = new char[n];
+                    int index = 0;
+
+                    for (int div = n; div >= 1; div--)
+                    {
+                        int q = temp / div;
+                        int r = temp % div;
+                        ch[index] = sb[r];
+
+                        sb.Remove(r, 1);
+                        temp = q;
+                        index++;
+                    }
+                    string str = new string(ch);
+                    listOfStrings.Add(str);
+                }
+            }
+
+            // generate all permutations of a string
+            public static string[] performIter(string s)
+            {
+                permutationsIter(s);
                 return listOfStrings.ToArray();
             }
         }
--- Algorithms/Question1/Question1/Program.cs
+++ Algorithms/Question1/Question1/Program.cs
@@ -9,11 +9,26 @@ namespace Question1
             // welcome message
             Console.WriteLine("Permutations of a String using Iterative method and Recursion method\n");
 
-            // object instansiation
-            string[] recursive = Permutations.Recursive.perform("ABC");
-            
+            // object instansiation - recursive
+            string[] recursive = Permutations.Recursive.performRec("ABC");
+
             // display result
+            Console.Write("Recursive: ");
             foreach (var item in recursive)
+            {
+                Console.Write(item + " ");
+            }
+
+
+            Console.WriteLine("\n");
+
+
+            // object instansiation - iterative
+            string[] iterative = Permutations.Iterative.performIter("ABC");
+
+            // display result
+            Console.Write("Iterative: ");
+            foreach (var item in iterative)
             {
                 Console.Write(item + " ");
             }
