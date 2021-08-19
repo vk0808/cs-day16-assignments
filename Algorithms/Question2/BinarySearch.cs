04130944c3fa5bc739c4ba45a69da024c9be0b94
vk0808 <vk4765712@gmail.com>
2021-08-19 14:36:17 +0530

    add: binary search class

---
+++ Algorithms/Question2/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question2
+{
+    public class BinarySearch
+    {
+        string _search;
+        string[] _array;
+
+        public BinarySearch(string search, string[] array)
+        {
+            this._search = search;
+            this._array = array;
+        }
+
+        private bool Search(int high, int low)
+        {
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (this._search == this._array[mid])
+                {
+                    return true;
+                }
+                else if (this._search.CompareTo(this._array[mid]) < 0)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return false;
+        }
+
+        public void performSearch()
+        {
+            int high = this._array.Length - 1;
+            int low = 0;
+            bool res = Search(high, low);
+
+            if (res)
+            {
+                Console.WriteLine("Found\n");
+            }
+            else
+            {
+                Console.WriteLine("Not found\n");
+            }
+        }
+    }
+}
--- Algorithms/Question2/Program.cs
+++ Algorithms/Question2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Question2
 {
@@ -6,7 +7,7 @@ namespace Question2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Binary Search the Word from Word List");
+            Console.WriteLine("Binary Search the Word from Word List\n");
         }
     }
 }
