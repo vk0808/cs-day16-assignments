ac7ad51b736eb05c34b60edbcd0ab624f6230332
vk0808 <vk4765712@gmail.com>
2021-08-19 21:24:41 +0530

    add: read file & display output

--- Algorithms/Question4/Program.cs
+++ Algorithms/Question4/Program.cs
@@ -7,7 +7,27 @@ namespace Question4
     {
         static void Main(string[] args)
         {
+            // welcome message
             Console.WriteLine("Bubble Sort\n");
+
+
+            // Path to txt file
+            const string PATH = "../../../WordList.txt";
+
+            // get array of words separated by ',' from the file
+            string[] words = File.ReadAllText(PATH).Split(",");
+
+            // print before sorting
+            Console.WriteLine("Unsorted array: ");
+            BubbleSort.Print(words);
+            Console.WriteLine("\n");
+
+            // sort the array
+            BubbleSort.SortArray(words);
+
+            // print after sorting
+            Console.WriteLine("Sorted array: ");
+            BubbleSort.Print(words);
         }
     }
 }
