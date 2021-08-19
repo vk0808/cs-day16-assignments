c770ff5a02d96e27a98fcf98ff01c7aa42f261c2
vk0808 <vk4765712@gmail.com>
2021-08-19 23:20:44 +0530

    add: welcome message

---
+++ Algorithms/Question7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime numbers[0 - 1000]\n");
+        }
+    }
+}
