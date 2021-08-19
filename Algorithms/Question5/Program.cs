7062c826ae8079c6be6ac559b1cffafbb26fe050
vk0808 <vk4765712@gmail.com>
2021-08-19 22:34:25 +0530

    add: read file & display output

--- Algorithms/Question5/Program.cs
+++ Algorithms/Question5/Program.cs
@@ -7,7 +7,26 @@ namespace Question5
     {
         static void Main(string[] args)
         {
+            // Welcome message
             Console.WriteLine("Merge Sort\n");
+
+            // Path to txt file
+            const string PATH = "../../../WordList.txt";
+
+            // get array of words separated by ',' from the file
+            string[] words = File.ReadAllText(PATH).Split(",");
+
+            // print before sorting
+            Console.WriteLine("Unsorted array: ");
+            MergeSort.Print(words);
+            Console.WriteLine("\n");
+
+            // sort the array
+            MergeSort.SortArray(words, 0, words.Length - 1);
+
+            // print after sorting
+            Console.WriteLine("Sorted array: ");
+            MergeSort.Print(words);
         }
     }
 }
