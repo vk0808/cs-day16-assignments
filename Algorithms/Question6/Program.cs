7992ad021136ee928a977bb1e525c6fa2233be5f
vk0808 <vk4765712@gmail.com>
2021-08-19 22:48:14 +0530

    add: welcome message

---
+++ Algorithms/Question6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check Anagrams\n");
+        }
+    }
+}
