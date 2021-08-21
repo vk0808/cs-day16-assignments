2efda06f2148810da058d3ee04b57f6382b15419
vk0808 <vk4765712@gmail.com>
2021-08-21 13:02:26 +0530

    add: guess the number by binary search

---
+++ Algorithms/Question10/GuessTheNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question10
+{
+    public class GuessTheNumber
+    {
+        // instance variables
+        private long guess = -1;
+        private long low = 0;
+        private long high;
+
+        const string TRUE = "true";
+
+        // constructor
+        public GuessTheNumber(long high)
+        {
+            this.high = high;
+        }
+
+        // Method to find the number while asking queries
+        public void Search()
+        {
+            while (guess != low)
+            {
+                long mid = low + (high - low) / 2;
+
+                Console.WriteLine($"\nIs your number -\ntrue: less than {mid}\nfalse: greater than {mid + 1}\n");
+                string query = Console.ReadLine();
+
+                if (query.ToLower() == TRUE)
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+
+                // Check if both low & high equal, stop the loop
+                if (low == high)
+                {
+                    guess = low;
+                }
+            }
+
+            // Print the guessed number
+            Console.WriteLine($"The number is {guess}");
+        }
+
+    }
+}
--- Algorithms/Question10/Program.cs
+++ Algorithms/Question10/Program.cs
@@ -6,7 +6,16 @@ namespace Question10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the number");
+            // Welcome message
+            Console.WriteLine("Guess the number\n");
+
+            // Get the number
+            Console.Write("Enter the scope range number: ");
+            long num = long.Parse(Console.ReadLine());
+
+            // Object instantisation
+            GuessTheNumber guess = new GuessTheNumber(num);
+            guess.Search();
         }
     }
 }
