b834308d223453a08359fffd4be98ebd11d430d4
vk0808 <vk4765712@gmail.com>
2021-08-21 13:23:40 +0530

    add: welcome message

---
+++ Algorithms/Question8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime numbers that are Palindrome and Anagram\n");
+        }
+    }
+}
