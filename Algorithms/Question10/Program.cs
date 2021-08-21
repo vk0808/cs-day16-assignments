9371324ff0839c677ddaf5b5016ada0f4ddc8297
vk0808 <vk4765712@gmail.com>
2021-08-21 12:12:04 +0530

    add: welcome message

---
+++ Algorithms/Question10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the number");
+        }
+    }
+}
