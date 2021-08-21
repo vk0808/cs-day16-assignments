0445ce094424e446256f5186265db1e989da2de0
vk0808 <vk4765712@gmail.com>
2021-08-21 14:33:22 +0530

    add: generic search method

---
+++ Algorithms/Question9/BinarySearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question9
+{
+    public class BinarySearch<T> where T : IComparable
+    {
+        // instance variable
+        T _search;
+        T[] _array;
+
+        // constructor
+        public BinarySearch(T search, T[] array)
+        {
+            this._search = search;
+            this._array = array;
+        }
+
+        // method to perform binary search
+        private bool Search(int high, int low)
+        {
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (this._search.CompareTo(this._array[mid]) == 0)
+                {
+                    return true;
+                }
+                else if (this._search.CompareTo(this._array[mid]) < 0)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return false;
+        }
+
+
+        
+
+        // method to call binary search and display result
+        public void performSearch()
+        {
+            int high = this._array.Length - 1;
+            int low = 0;
+            bool res = Search(high, low);
+
+            if (res)
+            {
+                Console.WriteLine("Found\n");
+            }
+            else
+            {
+                Console.WriteLine("Not found\n");
+            }
+        }
+    }
+}
---
+++ Algorithms/Question9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Question9
+{
+    class Program
+    {
+        public static void initiateBinarySearch()
+        {
+            // Path to txt file
+            const string PATH = "../../../WordList.txt";
+
+            // get array of words separated by ',' from the file
+            string[] words = File.ReadAllText(PATH).Split(",");
+
+            // sort the array
+            Array.Sort(words, StringComparer.InvariantCulture);
+
+            Console.WriteLine("\n");
+            // get the word to search from the user
+            Console.Write("Enter the word you want to search: ");
+            string searchKey = Console.ReadLine();
+
+            // object instantisation
+            BinarySearch<string> bs = new BinarySearch<string>(searchKey, words);
+
+            // call search method
+            bs.performSearch();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rewrite using Generics\n");
+
+            initiateBinarySearch();
+        }
+    }
+}
