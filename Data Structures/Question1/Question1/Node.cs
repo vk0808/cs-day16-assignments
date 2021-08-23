1805226ba26bec13e0e92ffff5683251707e4e29
vk0808 <vk4765712@gmail.com>
2021-08-23 18:06:51 +0530

    add: generic linkedlist

---
+++ Data Structures/Question1/Question1/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question1
+{
+	public class Node<T>
+	{
+		// instance variables
+		public T data;
+		public Node<T> next;
+
+		// constructor
+		public Node(T data)
+		{
+			this.data = (T)data; 
+			this.next = null;
+		}
+
+		// method that return data of a given node
+		public T Getdata()
+		{
+			return data;
+		}
+
+		// method that gets the next node
+		public Node<T> GetNext()
+		{
+			return next;
+		}
+
+		// method to set data in node
+		public void SetData(T newData)
+		{
+			this.data = newData;
+		}
+
+		// method to set next in node
+		public void SetNext(Node<T> newNext)
+		{
+			this.next = newNext;
+		}
+	}
+}
--- Data Structures/Question1/Question1/UnorderedList.cs
+++ Data Structures/Question1/Question1/UnorderedList.cs
@@ -4,7 +4,119 @@ using System.Text;
 
 namespace Question1
 {
-    public class UnorderedList
+    public class UnorderedList<T>
     {
+        internal Node<T> head;
+
+        // Method to add at first position
+        internal void AddFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.next = this.head;
+            this.head = node;
+            //Console.WriteLine($"{node.data} is added to linked list");
+        }
+
+        // Method to append at last position
+        internal void AddLast(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            //Console.WriteLine($"{node.data} is appended to linked list");
+        }
+
+        // Method to insert between the nodes
+        internal void InsertAfter(Node<T> prev_node, T data)
+        {
+            if (prev_node == null)
+            {
+                Console.WriteLine("Previous node cannot be empty");
+                return;
+            }
+            Node<T> node = new Node<T>(data);
+            node.next = prev_node.next;
+            prev_node.next = node;
+            //Console.WriteLine($"{node.data} is inserted into linked list");
+        }
+
+
+        // Method to delete node at first position
+        internal void DeleteFirst()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                return;
+            }
+            this.head = temp.next;
+            //Console.WriteLine("node is deleted from linked list");
+        }
+
+        // Method to delete node at first position
+        internal void DeleteLast()
+        {
+            Node<T> temp = this.head;
+
+            if (temp == null)
+            {
+                return;
+            }
+
+            if (temp.next == null)
+            {
+                return;
+            }
+
+            while (temp.next.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = null;
+            //Console.WriteLine("node is deleted from linked list");
+        }
+
+        // Method to search if the value exits or not
+        internal bool Search(T value)
+        {
+            Node<T> node = head;
+            while (node != null)
+            {
+                if (node.data.Equals(value))
+                {
+                    return true;
+                }
+                node = node.next;
+            }
+            return false;
+        }
+
+        // Method to display
+        internal void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
