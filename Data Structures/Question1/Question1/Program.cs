95600fb22019931125432d4ca8c82a0141f2d156
vk0808 <vk4765712@gmail.com>
2021-08-23 17:43:21 +0530

    add: read file

--- Data Structures/Question1/Question1/Program.cs
+++ Data Structures/Question1/Question1/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
 
 namespace Question1
 {
     class Program
     {
+        // method to read file
+        static string[] ReadFile(string path)
+        {
+            return File.ReadAllText(path).Split(",");
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("UnOrdered List");
+            // welcome message
+            Console.WriteLine("UnOrdered List\n");
+
+            // Path to txt file
+            const string PATH = "../../../WordList.txt";
+
+            // get array of words separated by ',' from the file
+            string[] words = ReadFile(PATH);
+
+            // print words
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
         }
     }
 }
